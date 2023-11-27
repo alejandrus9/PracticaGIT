4654df372eeac51373f8989362cc32972b2b1b60
alejandrus9 <alepenelas3@gmial.com>
2023-11-27 13:42:32 +0100

    Primer commit

---
+++ For1.php
@@ -0,0 +1,11 @@
+<!-- Ej1: Realiza un programa que muestra los números del 1 al 100 (ambos incluidos). -->
+<?php 
+   for($i = 1; $i <= 100; $i++) {
+      echo $i . " ";
+   }
+   //patata
+//pisa
+?>
+    
+
+
